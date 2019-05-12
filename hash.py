237d7a0dc1ec3bf3eae5be2bc68d57f9ac74289e
Bachu M <bachu_misha@mail.ru>
2019-05-12 22:39:48 +0300

    find_password

--- hash.py
+++ hash.py
@@ -2,27 +2,18 @@ import hashlib
 import pandas as pd
 import time
 
-def first_meth():
-	data = pd.read_csv('10000001.txt',header = None,names=['password'])
-	for i in data['password']:
-		print(i)
+h = 'EA580646D2BF8AF82895D22BF3C8F1B4'.lower()
 
-def second_meth():
-	with open('10000001.txt','r') as file:
-		test = file.read()
-	test = test.split('\n')
-	for i in test:
-		print(i)
+def first_meth():
+	data = pd.read_csv('1000000.txt',header = None,names=['password'])
+	for i in data['password']:
+		res = hashlib.md5(str(i).encode('utf-8')).hexdigest()
+		if h == res:
+			print(f'{i} = {res}')
+			break
 
 time_first_fun = time.time()
 first_meth()
 time_first_fun = time.time() - time_first_fun
 
-time_second_fun = time.time()
-second_meth()
-time_second_fun = time.time() - time_second_fun
-
-print(f'first time = {time_first_fun}\nsecond time = {time_second_fun}')
-
-# h = 'EA580646D2BF8AF82895D22BF3C8F1B4'
-# print(h.lower() == hashlib.md5('Z6NGH58o'.encode('utf-8')).hexdigest())
+print(f'first time = {time_first_fun}')
