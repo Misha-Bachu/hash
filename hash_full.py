ed473a498990b1c8787698caa675a95512303dce
Bachu M <bachu_misha@mail.ru>
2019-05-13 11:33:52 +0300

    add_simple_meth

--- hash_full.py
+++ hash_full.py
@@ -1,12 +1,10 @@
 import time
 import rarfile
-
 upp = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 file_name_1 = './Variant01_1.rar'
 file_name_2 = './Variant01_2.rar'
 rarfile.UNRAR_TOOL='./unrar.exe'
-
-def first_meth():
+def first_meth(filename):
 	time_first = time.time()
 	file = rarfile.RarFile(file_name_1)
 	for i0 in upp:
@@ -16,21 +14,26 @@ def first_meth():
 					word = i0+i1+i2+i3
 					try:
 						file.extractall(pwd=word, path='./Folder')
-						file_save('firs_answer.txt',word,(time.time() - time_first))
+						file_save('first_answer.txt',word,(time.time() - time_first),filename)
 						return
 					except rarfile.RarCRCError:
+						pass
+					except rarfile.RarWrongPassword:
 						pass
 					for i4 in upp:
 						try:
 							file.extractall(pwd=word+i4, path='./Folder')
-							file_save('firs_answer.txt',word+i4,(time.time() - time_first))
+							file_save('first_answer.txt',word+i4,(time.time() - time_first),filename)
 							return
 						except rarfile.RarCRCError:
 							pass
+						except rarfile.RarWrongPassword:
+							pass
 
-def file_save(filename,pas,t):
-	with open(filename, 'w+') as savefile:
-		savefile.write(f'{pas} = {t}')
-	print(f'{pas} = {t}')
+def file_save(filename,pas,t,arch):
+	with open(filename, 'a+') as savefile:
+		savefile.write(f'{arch}. password = {pas} time = {t}')
+	print(f'{arch}. password = {pas} time = {t}')
 
-first_meth()
+first_meth(file_name_1)
+first_meth(file_name_2)
---
+++ hash_simple.py
@@ -0,0 +1,24 @@
+import time
+import rarfile
+upp = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+file_name_1 = './folder_3.rar'
+rarfile.UNRAR_TOOL='./unrar.exe'
+def first_meth():
+	time_first = time.time()
+	file = rarfile.RarFile(file_name_1)
+	for i0 in upp:
+		for i1 in upp:
+			for i2 in upp:
+				try:
+					file.extractall(pwd=i0+i1+i2, path='./folder')
+					file_save('first_answer.txt',i0+i1+i2,(time.time() - time_first))
+					return
+				except rarfile.RarWrongPassword:
+					pass
+
+def file_save(filename,pas,t):
+	with open(filename, 'w+') as savefile:
+		savefile.write(f'{pas} = {t}')
+	print(f'{pas} = {t}')
+
+first_meth()
